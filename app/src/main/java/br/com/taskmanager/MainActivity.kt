516f889c70f2f8a2ef047a15de561b576aef3de4
Breno Ferreira <brenohenrique.oliveiraferreira@gmail.com>
2024-09-23 17:17:37 -0300

    Task Manager Tutorial

--- app/src/main/java/br/com/taskmanager/MainActivity.kt
+++ app/src/main/java/br/com/taskmanager/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +40,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TaskManager(modifier: Modifier = Modifier) {
-
+    val image = painterResource(R.drawable.ic_task_completed)
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = modifier
+        )
+        Text(
+            text = stringResource(R.string.task_completed_title),
+            fontWeight = FontWeight.Bold,
+            modifier = modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = stringResource(R.string.task_completed_description),
+            fontSize = 16.sp,
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
